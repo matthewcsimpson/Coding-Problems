569bd1e79d81e6ae3d78464582df48ebaa93aaeb
Matthew Simpson <mcsimpson+gh@gmail.com>
2022-11-20 16:33:19 -0800

    0017

---
+++ 0017-letter-combinations-of-phone-number.js
@@ -0,0 +1,74 @@
+// Given a string containing digits from 2-9 inclusive,
+// return all possible letter combinations that the number
+// could represent. Return the answer in any order.
+
+// A mapping of digits to letters (just like on the telephone
+// buttons) is given below. Note that 1 does not map to any letters.
+
+// 2 = a,b,c
+// 3 = d,e,f
+// 4 = g,h,i
+// 5 = j,k,l
+// 6 = m,n,o
+// 7 = p,q,r,s
+// 8 = t,u,v
+// 9 = w,x,y,z
+
+// Example 1:
+
+// Input: digits = "23"
+// Output: ["ad","ae","af","bd","be","bf","cd","ce","cf"]
+// Example 2:
+
+// Input: digits = ""
+// Output: []
+// Example 3:
+
+// Input: digits = "2"
+// Output: ["a","b","c"]
+
+// Constraints:
+
+// 0 <= digits.length <= 4
+// digits[i] is a digit in the range ['2', '9'].
+
+/**
+ * @param {string} digits
+ * @return {string[]}
+ */
+var letterCombinations = function (digits) {
+  const lettersObj = {
+    2: "abc",
+    3: "def",
+    4: "ghi",
+    5: "jkl",
+    6: "mno",
+    7: "pqrs",
+    8: "tuv",
+    9: "wxyz",
+  };
+  const result = [];
+
+  if (!digits) {
+    return result;
+  }
+
+  const doTheWork = (level, string) => {
+    if (string.length === digits.length) {
+      result.push(string);
+      return;
+    }
+
+    let number = digits[level];
+    let letters = lettersObj[number];
+
+    for (let i = 0; i < letters.length; i++) {
+      doTheWork(level + 1, string + letters[i]);
+    }
+  };
+  doTheWork(0, "");
+  return result;
+};
+
+console.log(letterCombinations("23"));
+console.log(letterCombinations("5678"));
