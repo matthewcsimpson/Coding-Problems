f34329826d430970ec6dcc3f1654d6e8209e7df6
Matthew Simpson <mcsimpson+gh@gmail.com>
2022-12-08 13:22:37 -0800

    tidying

--- 0018-pascals-triangle.js
+++ 0018-pascals-triangle.js
@@ -29,11 +29,16 @@
  * @return {number[][]}
  */
 var generate = function (numRows) {
+  // make a new array
   let triangle = [];
+  // start a loop that loops the specified number of times (numRows)
   for (let i = 0; i < numRows; i++) {
+    // make an array for row i in the triangle, at the next size up. row
     triangle[i] = new Array(i + 1);
+    // the first and last items in each row will always be 1, so fill them.
     triangle[i][0] = 1;
     triangle[i][i] = 1;
+    
     for (let j = 1; j < i; j++) {
       triangle[i][j] = triangle[i - 1][j - 1] + triangle[i - 1][j];
     }
