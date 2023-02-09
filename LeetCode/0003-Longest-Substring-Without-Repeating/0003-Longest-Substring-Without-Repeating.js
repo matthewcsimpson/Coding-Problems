f972a7e7e7daa4ef988f61f9db9ca4ad947708b0
Matthew Simpson <mcsimpson+gh@gmail.com>
2023-02-08 23:25:23 -0800

    folder structure refactoring

--- LeetCode/0003-Longest-Substring-Without-Repeating/0003-Longest-Substring-Without-Repeating.js
+++ LeetCode/0003-Longest-Substring-Without-Repeating/0003-Longest-Substring-Without-Repeating.js
@@ -1,28 +1,4 @@
-// 3. Longest Substring Without Repeating Characters
-// Medium
-// Given a string s, find the length of the longest substring without repeating characters.
 
-// Example 1:
-
-// Input: s = "abcabcbb"
-// Output: 3
-// Explanation: The answer is "abc", with the length of 3.
-// Example 2:
-
-// Input: s = "bbbbb"
-// Output: 1
-// Explanation: The answer is "b", with the length of 1.
-// Example 3:
-
-// Input: s = "pwwkew"
-// Output: 3
-// Explanation: The answer is "wke", with the length of 3.
-// Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
-
-// Constraints:
-
-// 0 <= s.length <= 5 * 104
-// s consists of English letters, digits, symbols and spaces.
 
 /**
  * @param {string} s
@@ -47,7 +23,7 @@ const lengthOfLongestSubstring = (s) => {
     map.set(s[i], i);
 
     // the length of the max substring is greater than the current stored maximum,
-    // assign that vale as the maximum
+    // assign that value as the maximum
     if (i - start + 1 > max) {
       max = i - start + 1;
     }
@@ -56,6 +32,7 @@ const lengthOfLongestSubstring = (s) => {
   return max;
 };
 
+// Tests
 console.log(lengthOfLongestSubstring("pwwkew")); //3
 console.log(lengthOfLongestSubstring("bbbbb")); //1
 console.log(lengthOfLongestSubstring("abcabcbb")); //3
--- LeetCode/0004-Median-Two-Sorted-Arrays/0004-Median-Two-Sorted-Arrays.js
+++ LeetCode/0004-Median-Two-Sorted-Arrays/0004-Median-Two-Sorted-Arrays.js
@@ -1,27 +1,3 @@
-// Given two sorted arrays nums1 and nums2 of size m and n respectively,
-// return the median of the two sorted arrays.
-
-// The overall run time complexity should be O(log (m+n)).
-
-// Example 1:
-// Input: nums1 = [1,3], nums2 = [2]
-// Output: 2.00000
-// Explanation: merged array = [1,2,3] and median is 2.
-
-// Example 2:
-// Input: nums1 = [1,2], nums2 = [3,4]
-// Output: 2.50000
-// Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.
-
-// Constraints:
-
-// - nums1.length == m
-// - nums2.length == n
-// - 0 <= m <= 1000
-// - 0 <= n <= 1000
-// - 1 <= m + n <= 2000
-// - -106 <= nums1[i], nums2[i] <= 106
-
 /**
  * @param {number[]} nums1
  * @param {number[]} nums2
@@ -33,7 +9,6 @@ var findMedianSortedArrays = function (nums1, nums2) {
     return a - b;
   });
 
-  console.log(allnums);
   // if the array has an even length, we must average the middle two numbers and return that average.
   if (allnums.length % 2 === 0) {
     let index = Math.ceil(allnums.length / 2) - 1;
@@ -44,13 +19,13 @@ var findMedianSortedArrays = function (nums1, nums2) {
   }
 };
 
-// test arrays
+// Test Data
 let nums1 = [3];
 let nums2 = [-1, -2];
-
 let nums3 = [2, 3, 4];
 let nums4 = [7, 8, 9];
 
+// Tests
 console.log(findMedianSortedArrays(nums1, nums2));
 console.log(findMedianSortedArrays(nums2, nums3));
 console.log(findMedianSortedArrays(nums3, nums4));
