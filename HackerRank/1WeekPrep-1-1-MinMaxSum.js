87f3ce338d8e0747f394cce456492e376b974c78
Matthew Simpson <mcsimpson+gh@gmail.com>
2023-02-16 00:02:11 -0800

    .

--- HackerRank/1WeekPrep-1-1-MinMaxSum.js
+++ HackerRank/1WeekPrep-1-1-MinMaxSum.js
@@ -3,10 +3,7 @@
 // minimum and maximum values as a single line of two space-separated long integers.
 // Example
 
-// The minimum sum is  and the maximum sum is . The function prints
-// 16 24
 // Function Description
-// Complete the miniMaxSum function in the editor below.
 // miniMaxSum has the following parameter(s):
 // arr: an array of  integers
 // Print
@@ -25,22 +22,10 @@
 // 1 2 3 4 5
 // Sample Output
 // 10 14
-// Explanation
-// The numbers are , , , , and . Calculate the following sums using four of the five integers:
-// Sum everything except , the sum is .
-// Sum everything except , the sum is .
-// Sum everything except , the sum is .
-// Sum everything except , the sum is .
-// Sum everything except , the sum is .
+
 // Hints: Beware of integer overflow! Use 64-bit Integer.
 
-/*
- * Complete the 'miniMaxSum' function below.
- *
- * The function accepts INTEGER_ARRAY arr as parameter.
- */
-
-function miniMaxSum(arr) {
+const miniMaxSum = (arr) => {
   let sum = 0;
   for (let i = 0; i < arr.length; i++) {
     sum += arr[i];
@@ -56,8 +41,8 @@ function miniMaxSum(arr) {
       smallest = sum - arr[j];
     }
   }
-  console.log(smallest + " " + biggest);
-}
+  return smallest + ", " + biggest;
+};
 
 console.log(miniMaxSum([1, 2, 3, 4, 5])); // expected 10 14
 console.log(miniMaxSum([7, 69, 2, 221, 8974])); // expected 299 9271
