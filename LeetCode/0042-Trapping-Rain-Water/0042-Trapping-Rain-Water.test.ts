71af02ebcc49860f72bd61db0f365ff56a1a4e13
Matthew Simpson <mcsimpson+gh@gmail.com>
2023-02-26 23:20:42 -0800

    0042 additional test

--- LeetCode/0042-Trapping-Rain-Water/0042-Trapping-Rain-Water.test.ts
+++ LeetCode/0042-Trapping-Rain-Water/0042-Trapping-Rain-Water.test.ts
@@ -2,6 +2,8 @@ import { expect, test } from "@jest/globals";
 import trap from "./0042-Trapping-Rain-Water";
 
 test("0042-Trapping-Rain-Water", () => {
-  expect(trap([0,1,0,2,1,0,1,3,2,1,2,1])).toBe(6);
-  expect(trap([4,2,0,3,2,5])).toBe(9);
+  expect(trap([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1])).toBe(6);
+  expect(trap([4, 2, 0, 3, 2, 5])).toBe(9);
+  expect(trap([0, 1, 2, 1, 0])).toBe(0);
+  expect(trap([4,3,2,1,0,1,2,3,4])).toBe(16)
 });
