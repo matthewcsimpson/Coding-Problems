a4ece78b5f3158840eba5c1fc9b77e7e21fe0bf2
Matthew Simpson <mcsimpson+gh@gmail.com>
2023-03-06 19:02:25 -0800

    hackerrank time conversion minor refactor

--- HackerRank/Time-Conversion/Time-Conversion.js
+++ HackerRank/Time-Conversion/Time-Conversion.js
@@ -1,41 +1,3 @@
-// Given a time in -hour AM/PM format, convert it to military (24-hour) time.
-// Note: - 12:00:00AM on a 12-hour clock is 00:00:00 on a 24-hour clock.
-// - 12:00:00PM on a 12-hour clock is 12:00:00 on a 24-hour clock.
-// Example
-
-// s = '12:01:00PM'
-// Return '12:01:00'.
-
-// s = '12:01:00AM'
-// Return '00:01:00'.
-
-// Function Description
-// Complete the timeConversion function in the editor below. It should return a new string
-// representing the input time in 24 hour format.
-
-// timeConversion has the following parameter(s):
-// - string s: a time in  hour format
-// Returns
-// - string: the time in  hour format
-
-// Input Format
-// A single string  that represents a time in -hour clock format (i.e.:  or ).
-// Constraints
-// All input times are valid
-
-// Sample Input
-// 07:05:45PM
-
-// Sample Output
-// 19:05:45
-
-/*
- * Complete the 'timeConversion' function below.
- *
- * The function is expected to return a STRING.
- * The function accepts STRING s as parameter.
- */
-
 /**
  * Converts a well formed incoming string representing the
  * time in 12hr format to 24hr format.
@@ -44,6 +6,7 @@
  */
 function timeConversion(s) {
   const split = s.split(":");
+
   const am = split[2][2] === "A" ? true : false;
 
   if (am && parseInt(split[0]) === 12) {
@@ -53,7 +16,6 @@ function timeConversion(s) {
   }
 
   split[2] = split[2].slice(0, 2);
-
   return split.join(":");
 }
 
