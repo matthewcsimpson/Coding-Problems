06a1a69513898499aa0b8c17c5853e70d06cd8bb
Matthew Simpson <mcsimpson+gh@gmail.com>
2023-03-19 11:08:13 -0700

    leetcode 0020 test;

--- LeetCode/0020-Valid-Parenthesis/0020-Valid-Parenthesis.js
+++ LeetCode/0020-Valid-Parenthesis/0020-Valid-Parenthesis.js
@@ -1,30 +1,3 @@
-// Given a string s containing just the characters '(', ')', '{', '}', '[' and ']',
-// determine if the input string is valid.
-
-// An input string is valid if:
-
-// Open brackets must be closed by the same type of brackets.
-// Open brackets must be closed in the correct order.
-// Every close bracket has a corresponding open bracket of the same type.
-
-// Example 1:
-
-// Input: s = "()"
-// Output: true
-// Example 2:
-
-// Input: s = "()[]{}"
-// Output: true
-// Example 3:
-
-// Input: s = "(]"
-// Output: false
-
-// Constraints:
-
-// 1 <= s.length <= 104
-// s consists of parentheses only '()[]{}'.
-
 /**
  * @param {string} s
  * @return {boolean}
@@ -81,9 +54,5 @@ var isValid = function (s) {
   return checkArray.length > 0 ? false : true;
 };
 
-console.log(isValid("{}{}{}"));
-console.log(isValid("{}[]()"));
-console.log(isValid("{}{[]}"));
-console.log(isValid("{}{]}"));
-console.log(isValid("{[{[{[]}]}]}"));
-console.log(isValid("[{[([{)}])]}]"));
+module.exports = isValid;
+
